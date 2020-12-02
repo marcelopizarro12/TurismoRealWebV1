c8af29c8503b77895eae076895aa6ab5a7695908
marcelo <marcelo.pizarro312@gmail.com>
2020-12-02 18:03:22 -0300

    version 1

---
+++ WebFinal/Base/Iniciarsesion.aspx.cs
@@ -0,0 +1,27 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFinal.Base
+{
+    public partial class Iniciarsesion : System.Web.UI.Page
+    {
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ WebFinal/Funcionario/Funcionario.aspx.cs
@@ -0,0 +1,50 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFinal.Funcionario
+{
+    public partial class Funcionario : System.Web.UI.Page
+    {
+        OracleConnection ora = new OracleConnection("DATA SOURCE =localhost:1521/xe ; PASSWORD = 123 ; USER ID = admin1");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+
+
+            
+        }
+
+        void listadoactivo()
+        {
+
+  
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("../Funcionario/SeleccionarcehckinPrueba.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("../Funcionario/SeleccionarChekout.aspx");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("../Funcionario/IniciarSesion.aspx");
+        }
+    }
+}
